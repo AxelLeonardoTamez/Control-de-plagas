40015a3ecc9ba199c20ddd2100a43c710b087382
AxelLeonardoTamez <88898293+AxelLeonardoTamez@users.noreply.github.com>
2021-09-10 15:49:27 -0500

    Add files via upload

--- Control de plagas.py
+++ Control de plagas.py
@@ -11,6 +11,18 @@ def Mosca(hec):
     la = lq * 100 / diazinon
     return la
 
+def Pulgon(hec):
+    acefato = .7
+    agua = 120
+    kg_acefato = acefato*hec
+    print("los kg de acefato son", kg_acefato)
+    agua_hec = (kg_acefato*agua)/acefato
+    return agua_hec
+
+def Nematodos(hec):
+    fenamifos = 7
+    fenamifos_agua = fenamifos*hec
+    return fenamifos_agua
 
 def menu():
     print("1. Mildui")
@@ -26,50 +38,14 @@ def menu():
         hec = float(input("Dame el numero de hectareas "))
         res = Mosca(hec)
         print("Los litros de agua con diazinon necesarios son: ", res)
-
-
+    if op == 3:
+        hec = float(input("Dame el numero de hectareas "))
+        res = Pulgon(hec)
+        print("Los litros de agua con acefato necesarios son:", res)
+    if op == 4:
+        hec = float(input("Dame el numero de hectareas "))
+        res = Nematodos(hec)
+        print("Los litros de fenamifos necesarios son:", res)
 
 menu()
-
-"""
-Folpet = 150
-Semilla = 100
-
-print("Ha elegido mildui por tratamiento químico")
-print("Quimico es:Folpet")
-Kg = int(input("Kg de semillas que va a utilizar"))
-def kg_fertilizante_ha(kg, folpet, semilla)
-fertilizante = (Folpet*Kg)/Semilla
-return fertilizante
-
-
-
-Kg = int(input("Kg de semillas que va a utilizar"))
-fertilizante = (Folpet*Kg)/Semilla
-print("Los Kg necesarios de fertilizante por ha son",fertilizante)
-
-Acefato = .7
-Agua= 120
-
-print("Ha elegido pulgones por tratamiento químico")
-print("Quimico es: Acefato")
-Hectareas = float(input("Numero de hectareas que va a fertilizar"))
-Kg = (Acefato*Hectareas)
-print("Los kilogramos necesarios son",Kg)
-Litros= (Kg*Agua)/Acefato
-print("Los litros de agua por ha necesarios son", Litros)
-
-
-Diazinon = .9
-
-
-print("Ha elegido mosca de la remolacha por tratamiento químico")
-print("Quimico es:Diazinon")
-Hectareas = float(input("Numero de hectareas que va a fertilizar"))
-Litros = (Hectareas*Diazinon)
-print("Los litros de Diazinon nesesarios son", Litros)
-Lagua = (Litros*100)/Diazinon
-print("Diluidos en", Lagua)
-
-"""
 
